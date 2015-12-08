982cd948a2ae52bb58e6c0b8bcce4643ad5a3131
NL-BlackDragon <blackdragon.vrtoxin@gmail.com>
2015-12-08 03:42:37 +0100

    Update fingerprint to 6.0.1 MMB29M
    Signed-off-by: NL-BlackDragon <blackdragon.vrtoxin@gmail.com>

--- vrtoxin.mk
+++ vrtoxin.mk
@@ -13,8 +13,8 @@ PRODUCT_MANUFACTURER := Huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT="google/angler/angler:6.0/MDB08M/2353240:user/release-keys" \
-    PRIVATE_BUILD_DESC="angler-user 6.0 MDB08M 2353240 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29M/2431559:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29M 2431559 release-keys"
 
 # Boot animation
 PRODUCT_COPY_FILES += \
