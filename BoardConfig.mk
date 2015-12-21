764b02f71d52068dc5ffd5a5ddedf7b208e0eb50
Joshua Lynch <jl52741@gmail.com>
2015-12-21 12:16:34 -0600

    angler: TWRP brinup
    kang'd from TeamWin

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,3 +145,13 @@ EXTENDED_FONT_FOOTPRINT := true
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
 TARGET_KERNEL_CONFIG := saber_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+
+# TWRP
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INCLUDE_CRYPTO := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
