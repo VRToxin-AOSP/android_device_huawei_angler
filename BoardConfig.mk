ab071052ad120f1674fdab824b71b23d6c6158a2
NL-BlackDragon <blackdragon.vrtoxin@gmail.com>
2016-01-12 14:44:03 +0100

    BoardConfig: Build saberCore kernel
    Signed-off-by: NL-BlackDragon <blackdragon.vrtoxin@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,5 +141,5 @@ EXTENDED_FONT_FOOTPRINT := true
 
 # Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
-TARGET_KERNEL_CONFIG := ak_angler_defconfig
+TARGET_KERNEL_CONFIG := saber_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
