d68b1e44d1c5e7b375f6a8746d0083e9b52076ae
NL-BlackDragon <blackdragon.vrtoxin@gmail.com>
2016-07-07 09:07:07 +0200

    Update fingerprint to MTC19X
    Android Release 53 (MTC19X)
    
    Signed-off-by: NL-BlackDragon <blackdragon.vrtoxin@gmail.com>

--- vrtoxin.mk
+++ vrtoxin.mk
@@ -13,8 +13,8 @@ PRODUCT_MANUFACTURER := Huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC19V/2862947:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC19V 2862947 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC19X/2960136:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC19X 2960136 release-keys"
     
 # Boot animation
 PRODUCT_COPY_FILES += \
